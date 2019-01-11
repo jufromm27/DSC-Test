f9876aadf565a0fd771c029a677aa56805438bc3
jufromm27 <42849205+jufromm27@users.noreply.github.com>
2019-01-11 12:37:21 -0500

    Update CreateFileConfig.tests.ps1

--- CreateFileConfig.tests.ps1
+++ CreateFileConfig.tests.ps1
@@ -1,6 +1,5 @@
 Describe "Pester Test for CreateFileConfig" {
      Context "Ensure Present" {
- 
         It "Should exist in the desired location" {
             test-path "C:\DSC-Placed-This.txt" | Should Be $True
             }
